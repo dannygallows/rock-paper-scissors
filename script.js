a19497646a6f6b616635687e3af3ca2e744466fe
dannygallows <43937866+dannygallows@users.noreply.github.com>
2024-10-20 19:44:42 +0200

    Restructure the code a bit for readability

--- script.js
+++ script.js
@@ -12,14 +12,17 @@ function getHumanChoice (input) {
     else return "Invalid choice";
 }
 
+function playRound (humanChoice, computerChoice) {
 
-console.log(getComputerChoice());
-
-let input = prompt("rock paper scissors, type your choice fam");
-
-console.log(getHumanChoice(input));
+}
 
 let humanScore = 0;
 let computerScore = 0;
-
 console.log(`Your score is ${humanScore}, enemy score is ${computerScore}`);
+
+let input = prompt("rock paper scissors, type your choice fam");
+
+let player = getHumanChoice(input);
+let computer = getComputerChoice();
+
+playRound(player, computer);
